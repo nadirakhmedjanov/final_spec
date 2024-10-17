60a1ce854de1d7d9d48e6eb73ce6a76f32592db8
Nadir <akh_nodir@mail.ru>
2024-10-17 12:00:38 +0500

    add entrance file main.java

---
+++ tasks_14/HumanFriends/Main.java
@@ -0,0 +1,9 @@
+import view.View;
+import view.console_ui.ConsoleUI;
+
+public class Main {
+    public static void main(String[] args) {
+        View view = new ConsoleUI();
+        view.startWork();
+    }
+}
